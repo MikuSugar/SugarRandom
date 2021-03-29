1ae57878eb19e2580cb7a260cd3f17cc20c3a94b
MikuSugar <syfangjie@live.cn>
2021-03-29 16:10:51 +0800

    更新：add 随机数工具类

---
+++ src/main/java/me/mikusugar/randomsugar/app/utils/RandomUtil.java
@@ -0,0 +1,34 @@
+package me.mikusugar.randomsugar.app.utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
+/**
+ * author: fangjie email: syfangjie@live.cn date: 2021/3/29 11:03 上午
+ */
+public class RandomUtil {
+
+    private static final Random random=new Random();
+
+    public static <T> RandomService<T> getRandomData(List<T> list){
+        assert list!=null&&list.size()>0;
+        return () -> list.get(random.nextInt(list.size()));
+    }
+
+    public static <T>  RandomService<T> getRandomWeightData(Map<T,Integer> map){
+        assert map!=null&&map.size()>0;
+
+        final TreeMap<Integer,T> treeMap =new TreeMap<>();
+
+        int size=0;
+        for (Map.Entry<T,Integer> e:map.entrySet()){
+            size+=e.getValue();
+            treeMap.put(size-1,e.getKey());
+        }
+        final int len=size;
+        return () -> treeMap.ceilingEntry(random.nextInt(len)).getValue();
+    }
+
+}
