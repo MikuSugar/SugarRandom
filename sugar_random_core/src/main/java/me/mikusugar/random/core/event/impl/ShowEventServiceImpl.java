cbfb18b22715b8e9f28b73ab06c1149aa4766a7a
mikusugar <syfangjie@live.cn>
2021-08-12 18:24:05 +0800

    fix : echo

--- sugar_random_core/src/main/java/me/mikusugar/random/core/event/impl/ShowEventServiceImpl.java
+++ sugar_random_core/src/main/java/me/mikusugar/random/core/event/impl/ShowEventServiceImpl.java
@@ -12,28 +12,33 @@ import java.util.Map;
 @Service
 public class ShowEventServiceImpl implements ShowEventService {
 
-  private final Map<String, AbstractRandomService> randomServiceMap;
+    private final Map<String, AbstractRandomService> randomServiceMap;
 
-  public ShowEventServiceImpl(Map<String, AbstractRandomService> randomServiceMap) {
-    this.randomServiceMap = randomServiceMap;
-  }
+    public ShowEventServiceImpl(Map<String, AbstractRandomService> randomServiceMap) {
+        this.randomServiceMap = randomServiceMap;
+    }
 
-  @Override
-  public String getAllTypeInfo() {
-    StringBuilder res = new StringBuilder();
-    randomServiceMap.forEach(
-            (k, v) ->
-                    res.append(k)
-                            .append("\t 描述:")
-                            .append(v.helpText().replace(System.lineSeparator(), " "))
-                            .append(System.lineSeparator())
-    );
-    return res.toString();
-  }
+    @Override
+    public String getAllTypeInfo() {
+        StringBuilder res = new StringBuilder();
+        randomServiceMap.forEach(
+                (k, v) ->
+                        res.append(k)
+                                .append("\t 描述:")
+                                .append(v.helpText().replace(System.lineSeparator(), " "))
+                                .append(System.lineSeparator())
+        );
+        return res.toString();
+    }
 
-  @Override
-  public String echo(String name, Map<String, String> aliasMap) {
-    return null;
-  }
+    @Override
+    public String echo(String name, Map<String, String> aliasMap) {
+        if (name == null || name.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            aliasMap.forEach((k, v) -> sb.append(k).append("     \t").append(v).append(System.lineSeparator()));
+            return sb.toString();
+        }
+        return name + " " + aliasMap.get(name);
+    }
 
 }
